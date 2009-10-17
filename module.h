204a659227c0ef1e93013740a0a6d5d39631f47a
Jonathan Thomas (The man) <echidnaman@gmail.com>
2009-10-17 17:01:07 -0400

    Standardize Copyright headers, use copyright symbol throughout I added myself to several of the event-derived classes, and Harald to most of the other classes he refactored

--- module.h
+++ module.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2009 by Jonathan Thomas <echidnaman@kubuntu.org>        *
+ *   Copyright © 2009 by Jonathan Thomas <echidnaman@kubuntu.org>          *
  *                                                                         *
  *   This program is free software: you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
--- notificationhelper.h
+++ notificationhelper.h
@@ -1,5 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2009 by Jonathan Thomas <echidnaman@kubuntu.org>        *
+ *   Copyright © 2009 by Jonathan Thomas <echidnaman@kubuntu.org>          *
+ *   Copyright © 2009 Harald Sitter <apachelogger@ubuntu.com>              *
  *                                                                         *
  *   This program is free software: you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
