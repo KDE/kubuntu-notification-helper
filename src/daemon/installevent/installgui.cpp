6332c8f31716333d5d6d16e19aabac24309fe5fe
Jonathan Thomas <echidnaman@kubuntu.org>
2010-10-21 14:21:08 -0400

    Update my copyright year details

--- src/daemon/installevent/installgui.cpp
+++ src/daemon/installevent/installgui.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright © 2009 Jonathan Thomas <echidnaman@kubuntu.org>             *
+ *   Copyright © 2009-2010 Jonathan Thomas <echidnaman@kubuntu.org>        *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or         *
  *   modify it under the terms of the GNU General Public License as        *
